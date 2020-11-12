b58333021a1766731a61da010754acdfb8b829d2
Tyler <4926565+tyler71@users.noreply.github.com>
2020-11-12 13:40:52 -0800

    init of models

--- app/convertimage/models.py
+++ app/convertimage/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class ConversionType(models.Model):
+    name = models.CharField(max_length=20)
+
+class Users(models.Model):
+    name         = models.CharField(max_length=100)
+    create_date  = models.DateTimeField()
+    token        = models.CharField(max_length=100)
+
+class ImageUnit(models.Model):
+    result       = models.CharField(max_length=100, blank=True)
+    images_used  = models.IntegerField()
+    conversion   = models.ForeignKey(ConversionType, null=True, on_delete=models.SET_NULL)
+    submitted    = models.DateTimeField()
+    submitted_by = models.ForeignKey(Users, on_delete=models.CASCADE)
+
