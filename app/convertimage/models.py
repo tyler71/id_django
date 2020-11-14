af00e1b72651787229274095cbf0b398c384ec07
Tyler <4926565+tyler71@users.noreply.github.com>
2020-11-13 17:49:09 -0800

    change home page text

--- app/convertimage/models.py
+++ app/convertimage/models.py
@@ -9,7 +9,7 @@ class ImageUnit(models.Model):
     result       = models.ImageField(upload_to='imgs')
     images_used  = models.IntegerField()
     conversion   = models.CharField(max_length=20)
-    submitted    = models.DateTimeField()
+    submitted    = models.DateTimeField(auto_now_add=True)
     # submitted_by = models.ForeignKey(Users, on_delete=models.CASCADE)
 
 # class SubmittedImage(models.Model):
