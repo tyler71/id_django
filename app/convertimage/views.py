81eb570d3f98c91f77a73e6cfa340fcea310704e
Tyler <4926565+tyler71@users.noreply.github.com>
2020-11-14 21:21:05 -0800

    recent images shown in session!

--- app/convertimage/views.py
+++ app/convertimage/views.py
@@ -10,42 +10,54 @@ from .utils import conversion_choices
 def home(request):
     ImageUnit.objects.filter()
     if request.method == 'POST':
-        context = process_image(request)
-        img_hash = context['img_hash']
+        data = _process_images(request.POST, request.FILES)
+        data.save()
         if request.session.get('related_images', None) is None:
             request.session['related_images'] = list()
-        request.session['related_images'].append(img_hash)
-        context['request'] = request
+        request.session['related_images'].append(data.img_hash)
+        session_rel_img = request.session['related_images']
+        related_images = _related_ordered_images(session_rel_img)
+        context = {
+            'related_images': related_images,
+            'new_image_form': NewImageForm(),
+        }
         return render(request, 'home.html', context)
     else:
         form = NewImageForm()
-    return render(request, 'home.html', {'new_image_form': form})
 
-def process_image(request):
-        form = NewImageForm(request.POST, request.FILES)
-        if form.is_valid():
-            submitted_images = [img for img in request.FILES.getlist('images')]
-            img_count = len(submitted_images)
-            selected_method = form.cleaned_data['type']
-            img = conversion_choices[selected_method](submitted_images)
-            converted_img = img.return_image()
-            converted_img_name = img.image_hash() + '.jpg'
-            data = ImageUnit()
-            data.hash = img.image_hash()
-            data.images_used = img_count
-            data.conversion = selected_method
-            data.submitted = datetime.now()
-            data.result.save(converted_img_name, InMemoryUploadedFile(
-                converted_img,
-                None,
-                converted_img_name,
-                'image/jpeg',
-                converted_img.tell(),
-                None,
-            ))
-            data.save()
-            content = {
-                'img_hash'  : img.img_hash(),
-                'img_object': data,
-            }
-            return content
+        if request.session.get('related_images', None) is None:
+            related_images = None
+        else:
+            session_rel_img = request.session['related_images']
+            related_images = _related_ordered_images(session_rel_img)
+        return render(request, 'home.html', {'related_images': related_images, 'new_image_form': form})
+
+def _related_ordered_images(session_images):
+    related_images = ImageUnit.objects.filter(img_hash__in=session_images)
+    ordered = related_images.order_by('-submitted')[:10]
+    return ordered
+
+def _process_images(post, files):
+    form = NewImageForm(post, files)
+    if form.is_valid():
+        submitted_images = [img for img in files.getlist('images')]
+        img_count = len(submitted_images)
+        selected_method = form.cleaned_data['type']
+        img = conversion_choices[selected_method](submitted_images)
+        converted_img = img.return_image()
+        converted_img_name = img.image_hash() + '.jpg'
+
+        data = ImageUnit()
+        data.hash = img.image_hash()
+        data.images_used = img_count
+        data.conversion = selected_method
+        data.submitted = datetime.now()
+        data.result.save(converted_img_name, InMemoryUploadedFile(
+            converted_img,
+            None,
+            converted_img_name,
+            'image/jpeg',
+            converted_img.tell(),
+            None,
+        ))
+        return data
