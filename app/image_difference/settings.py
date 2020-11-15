a45a0d814b0dbb81062957ed1f0dcbdb5c6f8925
Tyler <4926565+tyler71@users.noreply.github.com>
2020-11-15 12:29:17 -0800

    Image unit migrations

--- app/image_difference/settings.py
+++ app/image_difference/settings.py
@@ -23,7 +23,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', 'change-me')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = bool(os.environ.get('DEBUG', True) == 'False')
 DEBUG = bool(os.environ.get('DEBUG', False) == 'True')
 
 ALLOWED_HOSTS = os.environ.get('HOST', '').split()
