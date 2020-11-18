03cc7c176c3bc377dd1cbdaee41cb58729ac0f68
Tyler <4926565+tyler71@users.noreply.github.com>
2020-11-17 17:11:13 -0800

    disable and enable submit button as needed
    probably not as efficient as could be
    .

--- app/convertimage/static/script.js
+++ app/convertimage/static/script.js
@@ -1,9 +1,17 @@
 const FILESELECTED = document.querySelector("#id_images").files;
 const FILES = document.querySelector("#id_images");
-const SUBMITBUTTON = document.querySelector();
+const SUBMITBUTTON = document.querySelector("#add-image-button");
+const SUBMITERRORMSG = document.querySelector("#add-image-error-message");
 
 FILES.addEventListener('change', (event) => {
-    if (FILESELECTED > 0 && FILESELECTED < 4) {
-        console.log("no good!")
+    if (FILESELECTED.length > 0 && FILESELECTED.length < 3) {
+        SUBMITBUTTON.disabled = true;
+        SUBMITBUTTON.classList.add("disabled");
+        SUBMITERRORMSG.classList.remove("d-none");
+        SUBMITERRORMSG.innerHTML = "Select 3 or more images!"
+    } else {
+        SUBMITBUTTON.disabled = false;
+        SUBMITBUTTON.classList.remove("disabled");
+        SUBMITERRORMSG.classList.add("d-none");
     }
 });
