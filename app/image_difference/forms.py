d970697457084baae29f620441a6f85eb904992f
Tyler <4926565+tyler71@users.noreply.github.com>
2020-11-24 22:56:51 -0800

    Can login and signup
    improve dashboard to include name

--- app/image_difference/forms.py
+++ app/image_difference/forms.py
@@ -1,10 +1,12 @@
 from django import forms
 
 class LoginForm(forms.Form):
+    type     = forms.CharField(initial="login", widget=forms.HiddenInput)
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
 
 class SignupForm(forms.Form):
+    type     = forms.CharField(initial="signup", widget=forms.HiddenInput)
     username = forms.CharField()
     email    = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput)
--- app/image_difference/views.py
+++ app/image_difference/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import authenticate, logout, login
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
 from django.views import View
+from .models import User
 
 from convertimage.models import ImageUnit
 
@@ -12,6 +13,12 @@ from .forms import LoginForm, SignupForm
 log = logging.getLogger(__name__)
 
 class LoginPage(View):
+    def setup(self, request, *args, **kwargs):
+        # Make sure the session has a list of related images, even if it's empty
+        if request.session.get('related_images', None) is None:
+            request.session['related_images'] = list()
+        super(LoginPage, self).setup(request, *args, **kwargs)
+
     def get(self, request):
         log.info(f"is user authenticated? {request.user.is_authenticated}")
         if request.user.is_authenticated is True:
@@ -19,16 +26,42 @@ class LoginPage(View):
         else:
             return render(request, 'login.html', {'login_form': LoginForm(), "signup_form": SignupForm()})
     def post(self, request):
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = authenticate(username=username, password=password)
-        if user is None:
-            return render(request, 'login.html')
+        if request.POST["type"] == "login":
+            return self._login(request)
         else:
+            return self._signup(request)
+
+    def _login(self, request):
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            user = authenticate(username=username, password=password)
+            if user is None:
+                return render(request, 'login.html', {'login_form': LoginForm(), "signup_form": SignupForm()})
+            else:
+                login(request, user)
+                session_rel_img = request.session['related_images']
+                self.associate_images(session_rel_img, user)
+                return redirect('dashboard')
+        else:
+            return render(request, 'login.html', {'login_form': LoginForm(), "signup_form": SignupForm()})
+
+    def _signup(self, request):
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            log.info(form.cleaned_data)
+            username = form.cleaned_data["username"]
+            email    = form.cleaned_data["email"]
+            password = form.cleaned_data["password"]
+
+            user = User.objects.create_user(username=username, email=email, password=password)
             login(request, user)
             session_rel_img = request.session['related_images']
             self.associate_images(session_rel_img, user)
             return redirect('dashboard')
+        else:
+            return render(request, 'login.html', {'login_form': LoginForm(), "signup_form": SignupForm()})
 
     @staticmethod
     def associate_images(session_rel_images, user):
