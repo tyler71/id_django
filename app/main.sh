5690cc4d703b703e0c6061058875b33b8c4532e1
Tyler <4926565+tyler71@users.noreply.github.com>
2022-10-09 12:30:15 -0700

    undo some changes

--- app/main.sh
+++ app/main.sh
@@ -5,4 +5,4 @@
 ./manage.py collectstatic --noinput
 
 #/usr/bin/caddy start -config /etc/caddy/Caddyfile
-/usr/local/bin/gunicorn --bind 0.0.0.0:35252 --access-logfile '-' --config /etc/gunicorn.py image_difference.wsgi
+/usr/local/bin/gunicorn --bind 127.0.0.1:35252 --access-logfile '-' --config /etc/gunicorn.py image_difference.wsgi
